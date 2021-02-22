409412f2c122bfd63eb311964be1c1849a6acba4
swinder0161 <swinder0161@gmail.com>
2021-02-22 21:29:19 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,4 +2,4 @@
 
 sudo cp swi_vmnet_monitor /usr/local/bin/
 sudo cp swi.vmware_vmnet.monitor.plist /Library/LaunchAgents/
-sudo launchctl load ~/Library/LaunchAgents/swi.vmware_vmnet.monitor.plist
+sudo launchctl load /Library/LaunchAgents/swi.vmware_vmnet.monitor.plist
